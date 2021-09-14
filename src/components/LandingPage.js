7337972dece74058e8b9acb2cc10dc12eb9ff95b
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-09-14 13:35:04 -0500

    created LandingPage.js with some placeholder code

---
+++ src/components/LandingPage.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+
+export const LandingPage = () => {
+
+
+    return (
+        <>
+        <h1>My BuzzeeHive</h1>
+        
+        <h3>Track your important items/documents and free your BuzzeeBrain</h3>
+
+        <section>
+            <h4>I have x WhereBeez</h4>
+
+            <div>View all Chevalier Household's WhereBeez</div>
+
+            <div>Create New WhereBee</div>
+
+        </section>
+        </>
+    )
+}
