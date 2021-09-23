d068c074a55b7f51cf4c07bd30f0a5abfbd9b893
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-09-23 11:07:47 -0500

    styling landing

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom";
 import './LandingPage.css';
+import beehive from './images/Beehive.png';
+
 
 
 export const LandingPage = () => {
@@ -12,38 +14,39 @@ export const LandingPage = () => {
     useEffect(
         () => {
             fetch("http://localhost:8088/users?_expand=household")
-            .then(res => res.json())
-            .then( (data) => setUserHouseholds(data))
-            },
+                .then(res => res.json())
+                .then((data) => setUserHouseholds(data))
+        },
         []
     )
     useEffect(
         () => {
-            fetch("http://localhost:8088/whereBeez?_expand=user&_expand=item&_sort=lastUpdated&_order=desc") 
-            .then(res => res.json())
-            .then(
-                (data) => setWhereBeez(data))
-            },
+            fetch("http://localhost:8088/whereBeez?_expand=user&_expand=item&_sort=lastUpdated&_order=desc")
+                .then(res => res.json())
+                .then(
+                    (data) => setWhereBeez(data))
+        },
         []
     )
 
     //~~~~Code to build array containing only the last update of a given item~~~~//
     //~~~~Used here to get the length of the array of a given user's WBz~~~~//
     const uniqueWhereBeez = []
-    
+
     whereBeez.forEach((wb) => {
         const hasWhereBee = uniqueWhereBeez.find((uniqueWhereBee) => (
-          uniqueWhereBee.itemId === wb.itemId
+            uniqueWhereBee.itemId === wb.itemId
         ));
-      
+
         if (!hasWhereBee) {
-          uniqueWhereBeez.push(wb)
-          return uniqueWhereBeez;
+            uniqueWhereBeez.push(wb)
+            return uniqueWhereBeez;
         }
-      })
-    
+    })
+
     const whereBeezByUser = uniqueWhereBeez.filter((wb) => {
-        return wb.userId === currentUser})
+        return wb.userId === currentUser
+    })
 
     const foundUserHousehold = userHouseholds.find((userHousehold) => {
         return userHousehold.id === currentUser
@@ -52,22 +55,35 @@ export const LandingPage = () => {
 
     return (
         <>
-        <h1>{foundUserHousehold?.name}'s BuzzeeHive</h1>
+            <h1 className="landing__title">{foundUserHousehold?.name}'s BuzzeeHive</h1>
 
-        <div className="landing__description">Track your important items/documents and free your BuzzeeBrain</div>
-            
-        <h3>I have {whereBeezByUser.length} WhereBeez</h3>
+            <div className="landing__description">Track your important items/documents and free your BuzzeeBrain</div>
 
-        <section className="landing__options">
-            
-            <Link to="./wherebeez" className="landing__view">
-                <div className="landing__viewtext">
-                    View all {foundUserHousehold?.household?.name}'s WhereBeez
-                </div>
-            </Link>
-            <Link to="./wherebeez/create"><div className="landing__create">Create New WhereBee</div></Link>
+            <h3 className="landing__numbers">I have {whereBeezByUser.length} WhereBeez</h3>
 
-        </section>
+            <section className="landing__options">
+
+                <Link to="./wherebeez" className="landing__view" >
+                    <div className="card bg-white border border-white" style={{width:'400px'}}>
+                        <img src={beehive} className="card-img" alt="beehive" />
+                        <div className="card-img-overlay">
+                            <h5 className="card-title">View all {foundUserHousehold?.household?.name}'s WhereBeez</h5>
+                        </div>
+                    </div>
+        
+                </Link>
+                <Link to="./wherebeez/create">
+                <div className="card bg-white border border-white" style={{width:'400px'}}>
+                        <img src={beehive} className="card-img" alt="beehive" />
+                        <div className="card-img-overlay">
+                            <h5 className="card-title">Create New WhereBee</h5>
+                        </div>
+                    </div>
+
+                </Link>
+
+            </section>
         </>
     )
 }
+
